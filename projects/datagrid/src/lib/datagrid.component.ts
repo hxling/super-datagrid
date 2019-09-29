e0385bf89ee4bccfcd5ad435ae46ad98eb49daae
huangxiuling <huangxiuling@HUANGXIULING00>
2019-09-29 11:12:54 +0800

    set resizeLine's height

--- projects/datagrid/src/lib/datagrid.component.ts
+++ projects/datagrid/src/lib/datagrid.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, ValidatorFn } from '@angular/forms';
  * @Author: 疯狂秀才(Lucas Huang)
  * @Date: 2019-08-06 07:43:07
  * @LastEditors: 疯狂秀才(Lucas Huang)
- * @LastEditTime: 2019-09-29 10:56:38
+ * @LastEditTime: 2019-09-29 11:11:01
  * @QQ: 1055818239
  * @Version: v0.0.1
  */
@@ -1297,9 +1297,11 @@ export class DatagridComponent implements OnInit, OnDestroy, OnChanges, AfterCon
 
     onColumnResizeBegin(e: MouseEvent) {
         if (this.resizeProxy) {
+            this.render2.setStyle(document.body, 'cursor', 'e-resize');
             const proxy = this.resizeProxy.nativeElement;
             const proxyPosLeft = this.getResizeProxyPosLeft(e);
             this.render2.setStyle(proxy, 'left', proxyPosLeft + 'px');
+            this.render2.setStyle(proxy, 'height', (this.height - this.pagerHeight) + 'px');
             this.toggleResizeProxy();
         }
     }
@@ -1316,6 +1318,7 @@ export class DatagridComponent implements OnInit, OnDestroy, OnChanges, AfterCon
         e.preventDefault();
     }
     onColumnResizeEnd(e: MouseEvent, col: DataColumn) {
+        this.render2.removeStyle(document.body, 'cursor');
         const proxy = this.resizeProxy.nativeElement;
         this.toggleResizeProxy(false);
         this.resizeColumnInfo.proxyLineEdge = 0;
